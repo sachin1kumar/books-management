628b93baf33e63281051d0f73138d5c668739630
Sachin <sachinkmr375@gmail.com>
2021-07-03 23:03:32 +0530

    created log file in custom path.

--- src/main/java/com/books/management/booksmanagement/consumers/ShortlistedBookConsumer.java
+++ src/main/java/com/books/management/booksmanagement/consumers/ShortlistedBookConsumer.java
@@ -2,6 +2,10 @@ package com.books.management.booksmanagement.consumers;
 
 import com.books.management.booksmanagement.entities.ShortlistDetails;
 import com.books.management.booksmanagement.services.ShortlistedService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,16 +15,19 @@ public class ShortlistedBookConsumer {
 
     @Autowired
     private ShortlistedService shortlistedService;
+    private final Logger logger = LoggerFactory.getLogger(ShortlistedBookConsumer.class);
 
     @RabbitListener(queues = "${spring.rabbitmq.addShortListedQueue}")
-    public void consumeShortlistedDetails(final ShortlistDetails shortlistDetails) {
-        System.out.println("Message receive from queue: " + shortlistDetails);
+    public void consumeShortlistedDetails(final ShortlistDetails shortlistDetails) throws JsonProcessingException {
+        logger.info("Message received from queue for consumeShortlistedDetails: {}"
+                , new ObjectMapper().writeValueAsString(shortlistDetails));
         shortlistedService.addToShortlist(shortlistDetails);
     }
 
     @RabbitListener(queues = "${spring.rabbitmq.removeShortListedQueue}")
-    public void removeShortlistedBook(final Long shortListedId) {
-        System.out.println("Message receive from queue: " + shortListedId);
+    public void removeShortlistedBook(final Long shortListedId) throws JsonProcessingException {
+        logger.info("Message received from queue for removeShortlistedBook: {}"
+                , new ObjectMapper().writeValueAsString(shortListedId));
         shortlistedService.removeFromShortlist(shortListedId);
     }
 }
