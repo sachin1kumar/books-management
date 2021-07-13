f8fce4eaaf7ce8067db9e8f9e3944e7ac0e2ff8a
Sachin <sachinkmr375@gmail.com>
2021-07-13 22:36:56 +0530

    ShortlistedServiceTest unit test covered.

--- src/test/java/com/books/management/booksmanagement/unittests/services/ShortlistedServiceTest.java
+++ src/test/java/com/books/management/booksmanagement/unittests/services/ShortlistedServiceTest.java
@@ -3,7 +3,6 @@ package com.books.management.booksmanagement.unittests.services;
 import com.books.management.booksmanagement.entities.ShortlistDetails;
 import com.books.management.booksmanagement.repositories.ShortlistedRepository;
 import com.books.management.booksmanagement.services.ShortlistedService;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -11,9 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShortlistedServiceTest {
@@ -45,5 +46,22 @@ public class ShortlistedServiceTest {
         Long actualShortId = captor.getValue();
         Long expectedShortId = 1234900L;
         assertEquals(expectedShortId, actualShortId);
+    }
+
+    @Test
+    public void getAllShortListedDetailsByIdTest() {
+        ShortlistDetails firstShortlistedDetails = new ShortlistDetails(1234900L, 123L, 1234L);
+        ShortlistDetails secondShortlistedDetails = new ShortlistDetails(1234901L, 123L, 1235L);
+        List<ShortlistDetails> shortlistDetailsList = new ArrayList<>();
+        shortlistDetailsList.add(firstShortlistedDetails);
+        shortlistDetailsList.add(secondShortlistedDetails);
+        when(shortlistedRepository.findAll()).thenReturn(shortlistDetailsList);
+
+        List<ShortlistDetails> actualShortlistedDetails = shortlistedService.getAllShortListedDetailsById(123L);
+        Long actualBookId = actualShortlistedDetails.get(0).getBookId();
+        Long expectedBookId = 1234L;
+        int actualListSize = actualShortlistedDetails.size();
+        assertEquals(expectedBookId, actualBookId);
+        assertEquals(2, actualListSize);
     }
 }
